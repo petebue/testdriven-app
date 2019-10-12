3bc4a89034199c5d14d0d0294129b43996664e4f
Pete Buelow <buelow.peter@gmail.com>
2019-10-12 16:13:46 -0600

    Register and Login Routes.

---
+++ services/users/project/api/auth.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, jsonify, request
+from sqlalchemy import exc, or_
+
+from project.api.models import User
+from project import db, bcrypt
+
+auth_blueprint = Blueprint('auth', __name__)
+
+@auth_blueprint.route('/auth/register', methods=['POST'])
+def register_user():
+    # get post data
+    post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object)
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+    try:
+        user = User.query.filter(
+            or_(User.username == username, User.email == email)
+        ).first()
+        if not user:
+            new_user = User(
+                username=username,
+                email=email,
+                password=password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            auth_token = new_user.encode_auth_token(new_user.id)
+            response_object['status'] = 'success'
+            response_object['message'] = 'Successfully registered.'
+            response_object['auth_token'] = auth_token.decode()
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That user already exists.'
+            return jsonify(response_object), 400
+    except (exc.IntegrityError, ValueError):
+        db.session.rollback()
+        return jsonify(response_object), 400
+
+
+@auth_blueprint.route('/auth/login', methods=['POST'])
+def login_user():
+    post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object), 400
+    email = post_data.get('email')
+    password = post_data.get('password')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            auth_token = user.encode_auth_token(user.id)
+            if auth_token:
+                response_object['status'] = 'success'
+                response_object['message'] = 'Successfully logged in.'
+                response_object['auth_token'] = auth_token.decode()
+                return jsonify(response_object), 200
+        else:
+            response_object['message'] = 'User does not exist.'
+            return jsonify(response_object), 404
+    except Exception:
+        response_object['message'] = 'Try again.'
+        return jsonify(response_object), 500
